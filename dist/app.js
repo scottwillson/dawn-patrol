0617b7ef3bd8d9d82ddbfc5e7ea0ecc595153669
Scott Willson <scott.willson@gmail.com>
2015-06-23 14:37:41 -0700

    Add hard-coded password to app to make test pass

--- dist/app.js
+++ dist/app.js
@@ -10,6 +10,8 @@ var morgan = require('morgan');
 var pgpLib = require('pg-promise');
 var cn = {
   host: 'localhost',
+  user: 'dawn-patrol-test',
+  password: 'secret',
   database: 'dawn-patrol-test'
 };
 var pgp = pgpLib(cn);
