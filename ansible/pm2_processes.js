f6ebaaa325b173cc7aae3066e7f4301089d7f5d4
Scott Willson <scott.willson@gmail.com>
2015-07-06 16:40:34 -0700

    On second thought, don't create tmp dir for logs. Pass in ENV config instead.

--- ansible/pm2_processes.js
+++ ansible/pm2_processes.js
@@ -6,23 +6,26 @@
       "cwd"         : "/var/www/dawn-patrol",
       "instances"   : 0,
       "exec_mode"   : "cluster_mode",
-      "env": {
-        "NODE_CONFIG": { "echoServer": { "webServerLogFilePath": "/home/dawn-patrol/.pm2/logs/mock-rails-api-server-out-2.log" } }
-      },
+      "merge_logs"  : true
     },
     {
       "name"        : "echo_server",
       "script"      : "dist/echo_server.js",
       "cwd"         : "/var/www/dawn-patrol",
       "instances"   : 1,
-      "exec_mode"   : "cluster_mode"
+      "exec_mode"   : "cluster_mode",
+      "merge_logs"  : true,
+      "env": {
+        "NODE_CONFIG": { "echoServer": { "webServerLogFilePath": "/home/dawn-patrol/.pm2/logs/mock-rails-api-server-out.log" } }
+      }
     },
     {
       "name"        : "mock_rails_api_server",
       "script"      : "dist/mock_rails_api_server.js",
       "cwd"         : "/var/www/dawn-patrol",
       "instances"   : 1,
-      "exec_mode"   : "cluster_mode"
+      "exec_mode"   : "cluster_mode",
+      "merge_logs"  : true
     }
   ]
 }
