2ac2b2ba3e0580fa0c97c76648c8ab504a9023d1
Scott Willson <scott.willson@gmail.com>
2015-07-29 14:50:02 -0700

    Use actual event ID in results row rather than alwasy using 0

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,16 @@
 'use strict';
 
+var config = require('config');
 var express = require('express');
 var morgan = require('morgan');
-
 var Promise = require('bluebird');
-
 var pgpLib = require('pg-promise');
+var request = require('request-promise');
+
 var pgp = pgpLib({ promiseLib: Promise });
-var config = require('config');
 var db = pgp(config.get('database.connection'));
+
+var railsAppHost = config.get('endToEndTest.railsAppHost');
 
 var app = express();
 
@@ -18,7 +20,13 @@ if (process.env.NODE_ENV !== 'test') {
 }
 
 app.get('/events/:id/results.json', function (req, res) {
-  db.none('insert into results (id, event_id) values (0, 0)').then(res.end());
+  request.get('http://' + railsAppHost + '/events/' + req.params.id + '/results.json').then(function (response) {
+    return JSON.parse(response);
+  }).then(function () {
+    return db.none('insert into results (event_id) values ($1)', [req.params.id]);
+  }).then(function () {
+    return res.end();
+  });
 });
 
 app.get('/results.json', function (req, res) {
--- test/app_test.js
+++ test/app_test.js
@@ -5,7 +5,9 @@ process.env.NODE_ENV = 'test';
 var app = require('./app').app;
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
+var config = require('config');
 var expect = chai.expect;
+var nock = require('nock');
 var pgpLib = require('pg-promise');
 var Promise = require('bluebird');
 var request = require('supertest-as-promised');
@@ -13,11 +15,19 @@ var request = require('supertest-as-promised');
 chai.use(chaiAsPromised);
 
 var pgp = pgpLib({ promiseLib: Promise });
-var config = require('config');
 var db = pgp(config.get('database.connection'));
+
+var railsAppHost = config.get('endToEndTest.railsAppHost');
 
 function resultsCount() {
   return db.one('select count(*) from results')
+    .then(function(result) {
+      return parseInt(result.count);
+    });
+}
+
+function eventResultsCount(eventId) {
+  return db.one('select count(*) from results where event_id=$1', [eventId])
     .then(function(result) {
       return parseInt(result.count);
     });
@@ -32,16 +42,32 @@ describe('app', function() {
     return db.none('truncate results');
   });
 
-  describe('GET /events/:id/results.json', function() {
-    it('responds with status OK', function() {
-      Promise.settle([
-        request(app)
-          .get('/events/0/results.json')
-          .set('Accept', 'application/json')
-          .expect(200),
+  describe('GET /events/:id/results.json for a new event ID', function() {
+    var railsAppServer;
 
-        expect(resultsCount()).to.eventually.eq(1)
-      ]);
+    before(function() {
+      railsAppServer = nock('http://' + railsAppHost)
+        .get('/events/719/results.json')
+        .reply(200, {
+          'event_id': 719
+        });
+      return railsAppServer;
+    });
+
+    it('creates a new result in the DB', function() {
+        return expect(resultsCount()).to.eventually.eq(0)
+        .then(function() {
+          return request(app)
+          .get('/events/719/results.json')
+          .set('Accept', 'application/json')
+          .expect(200);
+        })
+        .then(function() { return expect(resultsCount()).to.eventually.eq(1); })
+        .then(function() { return expect(eventResultsCount(719)).to.eventually.eq(1); });
+    });
+
+    after(function() {
+      return railsAppServer.done();
     });
   });
 
@@ -67,8 +93,7 @@ describe('app', function() {
         .expect(200)
         .then(function() {
           return expect(resultsCount()).to.eventually.eq(0);
-        }
-        );
+        });
     });
   });
 });
