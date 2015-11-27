d0bae17688091aefc2d32d3a65da4ce75816bad5
Scott Willson <scott.willson@gmail.com>
2015-11-27 15:45:46 -0800

    Add event_id index

--- migrations/20151127174746-AddRailsIDIndex.js
+++ migrations/20151127174746-AddRailsIDIndex.js
@@ -1,7 +1,9 @@
 exports.up = (db, callback) => {
+  db.addIndex('results', 'results_event_id', ['event_id'], false, callback());
   db.addIndex('results', 'results_rails_id', ['rails_id'], true, callback());
 };
 
 exports.down = (db, callback) => {
+  db.removeIndex('results_event_id', callback());
   db.removeIndex('results_rails_id', callback());
 };
