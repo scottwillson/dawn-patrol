40bd8918f026bc956140d54fa780d51f1a9d0b55
Scott Willson <scott.willson@gmail.com>
2015-11-27 16:08:14 -0800

    Add category_id

--- migrations/20150730005917-results.js
+++ migrations/20150730005917-results.js
@@ -1,6 +1,7 @@
 exports.up = (db, callback) => {
   db.createTable('results', {
     id: { type: 'int', primaryKey: true, autoIncrement: true },
+    'category_id': { type: 'int' },
     'event_id': { type: 'int', notNull: true },
     'person_id': { type: 'int' },
     'rails_id': { type: 'int' },
