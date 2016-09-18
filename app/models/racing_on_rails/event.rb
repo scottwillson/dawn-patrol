1fd158de453e13219f4c816e4e4794353639c863
Scott Willson <scott.willson@gmail.com>
2016-09-17 17:22:30 -0700

    Default to Road discipline when importing

--- app/models/racing_on_rails/event.rb
+++ app/models/racing_on_rails/event.rb
@@ -4,4 +4,8 @@ class RacingOnRails::Event < ApplicationRecord
   def self.association=(value)
     establish_connection configurations["racing_on_rails"][value.downcase][Rails.env]
   end
+
+  def discipline
+    self[:discipline] || "Road"
+  end
 end
