57034e1d4a75cb9e06ae04f7e304ba92934f04be
Scott Willson <scott.willson@gmail.com>
2016-10-01 09:20:16 -0700

    Add results to imported data

--- racing_on_rails/db/wsba.sql
+++ racing_on_rails/db/wsba.sql
@@ -29,3 +29,21 @@ insert into racing_associations (name, short_name) values (
   'Washington State Bicycling Association',
   'WSBA'
 );
+
+insert into categories (id, name) values (
+  1,
+  "Senior Men"
+);
+
+insert into races (id, category_id, event_id) values (
+  1,
+  1,
+  1
+);
+
+insert into results (event_id, race_id, person_id, place) values (
+  1,
+  1,
+  1,
+  "1"
+);
