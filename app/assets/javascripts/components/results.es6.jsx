4c119ae06a495202eb28636a136d3ce4fc52aebf
Scott Willson <scott.willson@gmail.com>
2016-10-15 17:21:03 -0700

    Import all original created_at/updated_at

--- app/assets/javascripts/components/results.es6.jsx
+++ app/assets/javascripts/components/results.es6.jsx
@@ -12,9 +12,10 @@ class Results extends React.Component {
         <h2>{this.state.name}</h2>
         {this.cityState(this.state.city, this.state.state)}
         {this.parent(this.state.parent)}
-        <div className='starts-at'>{moment(this.state.startsAt).format('MMMM D, YYYY')}</div>
+        <div className='starts-at'>{this.dates(this.state.starts_at, this.state.children)}</div>
         <Children children={this.state.children}/>
         {this.state.categories.map(category => <Category key={category.id} {...category} />)}
+        <div className='updated-at'>Updated {moment(this.state.updatedAt).format('MMMM D, YYYY')}</div>
       </div>
     );
   }
@@ -36,6 +37,15 @@ class Results extends React.Component {
       return (<div className='city-state'>{R.join(', ', [city, state])}</div>);
     }
     return null;
+  }
+
+  dates(startsAt, children) {
+    if (children.length > 1) {
+      const sortByStartsAt = R.sortBy(R.prop(['starts_at']));
+      const sortedChildren = sortByStartsAt(children);
+      return `${moment(sortedChildren[0].starts_at).format('MMMM D, YYYY')}-${moment(sortedChildren[sortedChildren.length - 1].starts_at).format('MMMM D, YYYY')}`;
+    }
+    return moment(startsAt).format('MMMM D, YYYY');
   }
 
   parent(parent) {
