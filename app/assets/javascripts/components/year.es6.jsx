dfc30353a7544157e3755b9794b35e317f01c2e7
Scott Willson <scott.willson@gmail.com>
2016-10-26 09:16:45 -0700

    Use mobile-friendly nav

--- app/assets/javascripts/components/year.es6.jsx
+++ app/assets/javascripts/components/year.es6.jsx
@@ -1,7 +1,14 @@
 function Year(props) {
   return (
-      <li className="nav-item">
-        <a className="nav-link" href={props.year}>{props.year}</a>
-      </li>
+    <li className={props.liClassName}>
+      <a className={className(props)} href={`?year=${props.year}`}>{props.year}</a>
+    </li>
   );
+}
+
+function className(props) {
+  if (props.selected === props.year) {
+    return `nav-link active`;
+  }
+  return 'nav-link';
 }
