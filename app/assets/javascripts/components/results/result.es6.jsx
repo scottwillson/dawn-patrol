cebd29c933bf5b710bb9d2be774e0ed397f92dd8
Scott Willson <scott.willson@gmail.com>
2016-11-19 19:27:59 -0800

    Don't hide result columns on phones

--- app/assets/javascripts/components/results/result.es6.jsx
+++ app/assets/javascripts/components/results/result.es6.jsx
@@ -1,10 +1,10 @@
 Results.Result = function Result(props) {
   return (
     <tr>
-      <td className="hidden-sm-down place">{props.place}</td>
-      <td className="hidden-sm-down name">{props.person && props.person.name}</td>
-      <td className="hidden-sm-down points">{props.points}</td>
-      <td className="hidden-sm-down time">{props.time}</td>
+      <td className="place">{props.place}</td>
+      <td className="name">{props.person && props.person.name}</td>
+      <td className="points">{props.points}</td>
+      <td className="time">{props.time}</td>
     </tr>
   );
 }
