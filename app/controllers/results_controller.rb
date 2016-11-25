c6ddf76931f1e02ef88d623d0821c15df7646ca3
Scott Willson <scott.willson@gmail.com>
2016-11-24 19:31:51 -0800

    Cache event results JSON

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -5,12 +5,18 @@ class ResultsController < ApplicationController
     respond_to do |format|
       format.html
       format.json do
-        render json: results.as_json({ include: [ :category, { results: { include: :person, methods: :numeric_place } } ] })
+        render json: results_as_json
       end
     end
   end
 
   def results
     Events::FindResults.new(event_id: params[:event_id]).do_it!
+  end
+
+  def results_as_json
+    Rails.cache.fetch(@event.cache_key) do
+      results.as_json({ include: [ :category, { results: { include: :person, methods: :numeric_place } } ] })
+    end
   end
 end
