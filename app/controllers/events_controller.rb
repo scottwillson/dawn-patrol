005510f82e7d3eb4a3359c3f6a33c9944e5037ea
Scott Willson <scott.willson@gmail.com>
2016-12-13 22:15:06 -0500

    Add HTTP caching to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,18 +1,28 @@
 class EventsController < ApplicationController
   def index
     @year = (params[:year] || DawnPatrol::Association.current.year).to_i
+    @last_updated_event = Event.year(@year).order(:updated_at).last
 
-    respond_to do |format|
-      format.html
-      format.json do
-        events = Events::FindAll.new(discipline: @discipline, year: @year).do_it!
-
-        render json: {
-          link_groups: link_groups(events, Event.years, @year),
-          events: events.as_json(methods: :promoter_names),
-          year: @year
-        }
+    if stale?(@last_updated_event)
+      respond_to do |format|
+        format.html
+        format.json do
+          render json: events_as_json
+        end
       end
+    end
+  end
+
+  def events_as_json
+    years = Event.years
+    Rails.cache.fetch([ @last_updated_event&.cache_key, years, @year ]) do
+      events = Events::FindAll.new(discipline: @discipline, year: @year).do_it!
+
+      {
+        link_groups: link_groups(events, years, @year),
+        events: events.as_json(methods: :promoter_names),
+        year: @year
+      }
     end
   end
 
