c5842115a12637a34a1552a7d7b28441a691340a
Scott Willson <scott.willson@gmail.com>
2017-07-19 16:38:05 -0700

    Remove old AsyncEvents

--- client/app/bundles/AsyncEvents/EventsApp.js
+++
@@ -1,8 +0,0 @@
-import 'babel-polyfill';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import EventsRoot from './containers/EventsRoot';
-import ReactOnRails from 'react-on-rails';
-
-ReactOnRails.register({ EventsRoot });
