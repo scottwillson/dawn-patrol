0e072570bf3249bfa25c7c644cddafcca530972d
Scott Willson <scott.willson@gmail.com>
2017-07-20 06:44:17 -0700

    Remove old Public client bundle

--- client/app/bundles/Public/PublicApp.js
+++
@@ -1,8 +0,0 @@
-import 'babel-polyfill';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './containers/Root';
-import ReactOnRails from 'react-on-rails';
-
-ReactOnRails.register({ Root });
