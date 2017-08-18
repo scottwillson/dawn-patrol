2269beb14e81b46565a277365777bb6622a07588
Scott Willson <scott.willson@gmail.com>
2017-08-18 14:32:29 -0700

    Put e2e test in container

--- e2e-test/spec/end_to_end_spec.rb
+++ e2e-test/spec/end_to_end_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe "DawnPatrol", type: "feature" do
   it "shows a webpage" do
-    visit "http://localhost:8080/"
+    visit "http://web/"
     expect(page).to have_css "h2", text: "Dawn Patrol"
     expect(page).to have_css ".events", text: "4 events"
   end
