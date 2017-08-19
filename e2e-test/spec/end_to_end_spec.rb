0e8975472aedd576a34e29c5c50ee052d83fd2cb
Scott Willson <scott.willson@gmail.com>
2017-08-18 21:03:24 -0700

    Stub out import step

--- e2e-test/spec/end_to_end_spec.rb
+++ e2e-test/spec/end_to_end_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "DawnPatrol", type: "feature" do
     expect(page).to have_css "h2", text: "Dawn Patrol"
     expect(page).to have_css ".events", text: "0 events"
 
-    `bin/import`
+    # import
 
     visit "http://web/"
     expect(page).to have_css ".events", text: "4 events"
