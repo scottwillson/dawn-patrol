d1d88bf857d99a063a1355218eba557dbaf37eab
Scott Willson <scott.willson@gmail.com>
2017-08-30 07:31:22 -0700

    Retry in E2E test in case API server isn't up

--- e2e-test/spec/end_to_end_spec.rb
+++ e2e-test/spec/end_to_end_spec.rb
@@ -1,15 +1,23 @@
 require 'net/http'
 require 'spec_helper'
 
-RSpec.describe "DawnPatrol", type: "feature" do
-  it "shows a webpage" do
-    visit "http://web/"
-    expect(page).to have_css "h2", text: "Dawn Patrol"
-    expect(page).to have_css ".events", text: "0 events"
+RSpec.describe 'DawnPatrol', type: 'feature' do
+  it 'shows a webpage' do
+    visit 'http://web/'
+    expect(page).to have_css 'h2', text: 'Dawn Patrol'
+    expect(page).to have_css '.events', text: '0 events'
 
-    Net::HTTP.post_form URI('http://api:8080/rails/copy'), {}
+    3.times do
+      begin
+        Net::HTTP.post_form URI('http://api:8080/rails/copy'), {}
+        break
+      rescue
+        sleep 1
+        retry
+      end
+    end
 
-    visit "http://web/"
-    expect(page).to have_css ".events", text: "2 events"
+    visit 'http://web/'
+    expect(page).to have_css '.events', text: '2 events'
   end
 end
